89164fd6d89cc9ac35c855db5c8d6d6b076de012
aposysoev <aposysoev@solit-clouds.ru>
2023-01-20 18:20:34 +0300

    add input in cart component

--- projects/cart/src/lib/cart.component.ts
+++ projects/cart/src/lib/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'lib-cart',
@@ -13,5 +13,5 @@ import { Component } from '@angular/core';
   ]
 })
 export class CartComponent {
-
+  @Input() text: string = ''
 }
